446206427edf8c9c6f70ff7869b73e98f056c9ae
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-10 12:31:45 +0600

    Initial Commit

---
+++ List App/List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  List_AppApp.swift
+//  List App
+//
+//  Created by Rezaul Islam on 10/7/23.
+//
+
+import SwiftUI
+
+@main
+struct List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
