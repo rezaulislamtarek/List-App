ead52ac3b8c0435c3a2a3c843e1ae77ec8f7d20c
rezaulislamtarek <mrezaulislamtarek.t@gmail.com>
2023-07-11 14:48:30 +0600

    searchable list

--- List App/ContentView.swift
+++ List App/ContentView.swift
@@ -7,25 +7,67 @@
 
 import SwiftUI
 
+struct Place : Identifiable{
+    let id = UUID()
+    let name :String
+    let photo :String
+}
+
+func getPlaces() -> [Place] {
+    /*
+     "Liberation-War-Museum"
+     "Mahasthangarh"
+     "Mainamati 1"
+     "National-Memorial"
+     "PanamCity"
+     "Shaheed-Minar
+     ""Shat-Gombuj-Mosque"
+     "Star-Mosque"
+
+     */
+    return [
+        Place(name: "Star Mosque", photo: "Star-Mosque"),
+        Place(name: "Shat Gombuj Mosque", photo: "Shat-Gombuj-Mosque"),
+        Place(name: "Shaheed Minar", photo: "Shaheed-Minar"),
+        Place(name: "PanamCity", photo: "PanamCity"),
+        Place(name: "National Memorial", photo: "National-Memorial"),
+        Place(name: "Mainamati", photo: "Mainamati 1"),
+        Place(name: "Mahasthangarh", photo: "Mahasthangarh"),
+        Place(name: "Liberation War Museum", photo: "Liberation-War-Museum"),
+    ]
+}
  
 
 struct ContentView: View {
-    
-    @State var dateList : [Date] = []
-    
+    @State var places = getPlaces()
+    @State var search = ""
+ 
     var body: some View {
         NavigationView{
-            List(dateList, id: \.self){ date in
-                Text("\(date)")
-            }
-            .refreshable {
-                DispatchQueue.main.asyncAfter(deadline:.now()+1.0){
-                    dateList.append(Date())
+            List(places) { place in
+                HStack {
+                    Image(place.photo)
+                        .resizable()
+                        .frame(maxWidth: 60, maxHeight: 60)
+                    .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+                    Text(place.name)
+                        .font(.title3)
                 }
+                .searchable(text: $search, placement: .navigationBarDrawer(displayMode: .always))
+                    .onChange(of: search, perform: { value in
+                        if !value.isEmpty && value.count >= 1{
+                            places = places.filter{
+                                $0.name.lowercased().hasPrefix(value.lowercased())
+                            }
+                        }else{
+                            places = getPlaces()
+                        }
+                    } )
+                    .listRowSeparator(.hidden)
+                    
+                
             }
-            .navigationTitle("Pull To refresh")
-            .listStyle(.inset)
-             
+            .navigationTitle("Places")
         }
         
     }
